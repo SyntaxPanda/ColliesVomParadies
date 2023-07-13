12f5aa489ec164c636bb35f0052cc46db7026089
SyntaxPanda <rodetimy1996@yahoo.com>
2023-07-13 19:23:07 +0200

    Route to HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-
+    <Routes>
+      <Route path={"/"} element={<HomePage/>}/>
+    </Routes>
     </div>
   );
 }
