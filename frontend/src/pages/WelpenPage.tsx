00819a334ab3d9cf3d2dd043e7421cf8582f33b4
SyntaxPanda <rodetimy1996@yahoo.com>
2023-07-13 22:02:14 +0200

    marker for every year dogs got kids

--- frontend/src/pages/WelpenPage.tsx
+++ frontend/src/pages/WelpenPage.tsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function WelpenPage() {
     return (
-        <div></div>
+        <div>
+            <div>23</div>
+            <div>21</div>
+            <div>20</div>
+            <div>19</div>
+            <div>18</div>
+            <div>17</div>
+            <div>16</div>
+            <div>15</div>
+            <div>14</div>
+            <div>12</div>
+            <div>11</div>
+            <div>10</div>
+            <div>09</div>
+            <div>08</div>
+            <div>07</div>
+            <div>06</div>
+        </div>
     );
 }
